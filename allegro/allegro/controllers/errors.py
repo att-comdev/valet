ed66dd42f81e1763d2834029c3ef6af031d069e6
Joe D'Andrea <jdandrea@research.att.com>
2016-05-12 19:50:20 +0000

    Improved error reporting structure. README updated.

--- allegro/allegro/controllers/errors.py
+++ allegro/allegro/controllers/errors.py
@@ -18,6 +18,8 @@
 
 '''Errors'''
 
+from webob.exc import status_map
+
 from allegro.i18n import _
 
 from pecan import expose, request, response
@@ -27,31 +29,30 @@ from pecan import expose, request, response
 
 def error_wrapper(func):
     '''Error decorator.'''
-    def func_wrapper(self, **kwargs):
-        '''Wrapper function for error decorator.'''
-        # Call the controller method
-        kwargs = func(self, **kwargs)
+    def func_wrapper(self):
+        '''Wrapper.'''
 
-        # Prep the actual error
-        # Modeled after Apple's error APIs at present.
-        # TODO: Use OpenStack format?
-        message = kwargs.get('message', _('Undocumented error'))
-        internal_message = kwargs.get('internal', response.status)
-        status = kwargs.get('status', response.status_code)
-        info = kwargs.get('info', _('No remediation available'))
+        kwargs = func(self)
+        status = status_map.get(response.status_code)
+        message = getattr(status, 'explanation', '')
+        explanation = request.context.get('error_message', message)
+        error_type = status.__name__
+        title = status.title
+        traceback = getattr(kwargs, 'traceback', None)
 
-        # TODO: Support multiple errors?
+        # Modeled after Heat's format
         return {
-            "errors": [{
-                "userMessage": message,
-                "internalMessage": internal_message,
-                "code": status,
-                "info": info,
-            }]
+            "explanation": explanation,
+            "code": response.status_code,
+            "error": {
+                "message": message,
+                "traceback": traceback,
+                "type": error_type,
+            },
+            "title": title,
         }
     return func_wrapper
 
-# TODO: Pass in the rest of kwargs along with the message
 class ErrorsController(object):
     '''
     Errors Controller
@@ -62,25 +63,21 @@ class ErrorsController(object):
     @error_wrapper
     def schema(self):
         '''400'''
-        msg = str(request.validation_error)
+        request.context['error_message'] = str(request.validation_error)
         response.status = 400
-        return dict(message=msg)
+        return request.context.get('kwargs')
 
     @expose('json')
     @error_wrapper
-    def invalid(self, **kw):
+    def invalid(self):
         '''400'''
-        msg = kw.get(
-            'error_message',
-            _('invalid request')
-        )
         response.status = 400
-        return dict(message=msg)
+        return request.context.get('kwargs')
 
     @expose()
     def unauthorized(self):
         '''401'''
-        # Don't return any implementation details.
+        # This error doesn't use the wrapper on purpose.
         response.status = 401
         response.content_type = 'text/plain'
         response.body = _('Authentication required')
@@ -88,71 +85,47 @@ class ErrorsController(object):
 
     @expose('json')
     @error_wrapper
-    def forbidden(self, **kw):
+    def forbidden(self):
         '''403'''
-        msg = kw.get(
-            'error_message',
-            _('forbidden')
-        )
         response.status = 403
-        return dict(message=msg)
+        return request.context.get('kwargs')
 
     @expose('json')
     @error_wrapper
-    def not_found(self, **kw):
+    def not_found(self):
         '''404'''
-        msg = kw.get(
-            'error_message',
-            _('resource was not found')
-        )
         response.status = 404
-        return dict(message=msg)
+        return request.context.get('kwargs')
 
     @expose('json')
     @error_wrapper
-    def not_allowed(self, **kw):
+    def not_allowed(self):
         '''405'''
-        msg = kw.get(
-            'error_message',
-            _('method not allowed')
-        )
         kwargs = request.context.get('kwargs')
         if kwargs:
             allow = kwargs.get('allow', None)
             if allow:
                 response.headers['Allow'] = allow
         response.status = 405
-        return dict(message=msg)
+        return kwargs
 
     @expose('json')
     @error_wrapper
-    def conflict(self, **kw):
+    def conflict(self, **kwargs):
         '''409'''
-        msg = kw.get(
-            'error_message',
-            _('conflict')
-        )
         response.status = 409
-        return dict(message=msg)
+        return request.context.get('kwargs')
 
     @expose('json')
     @error_wrapper
-    def server_error(self, **kw):
+    def server_error(self, **kwargs):
         '''500'''
-        msg = kw.get(
-            'error_message',
-            _('server error'),
-        )
         response.status = 500
-        return dict(message=msg)
+        return request.context.get('kwargs')
 
     @expose('json')
     @error_wrapper
-    def unavailable(self, **kw):
+    def unavailable(self, **kwargs):
         '''503'''
-        msg = kw.get(
-            'error_message',
-            _('service unavailable'),
-        )
         response.status = 503
-        return dict(message=msg)
+        return request.context.get('kwargs')

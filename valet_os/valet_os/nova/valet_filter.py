5c8eabea037d8a30b8d6ba81be1cd300079de8e8
Joe D'Andrea <jdandrea@research.att.com>
2016-05-19 23:01:48 -0400

    Nova ValetFilter now supports ad-hoc placements.
    If a resource is being scheduled and the request did not
    come from heat (no stack lifecycle scheduler hints), place
    the resource on-the-fly. The stack id and orchestration id
    will be set to the physical resource id in this case, and
    an ad hoc resource will be sent to valet, containing the
    flavor and no other properties.

--- valet_os/valet_os/nova/valet_filter.py
+++ valet_os/valet_os/nova/valet_filter.py
@@ -92,37 +92,90 @@ class ValetFilter(filters.BaseHostFilter):
         cfg.CONF.register_group(opt_group)
         cfg.CONF.register_opts(opts, group=opt_group)
 
-    # TODO: Factor out common code to a qosorch library
+    # TODO: Factor out common code between this and the cinder filter
     def filter_all(self, filter_obj_list, filter_properties):
         '''Filter all hosts in one swell foop'''
         hints_key = 'scheduler_hints'
         uuid_key = 'heat_resource_uuid'
 
+        ad_hoc = False
         yield_all = False
         location = None
         uuid = None
 
-        # If we don't have hints to process, yield (pass) all hosts
-        # so other plugins have a fair shot. TODO: This will go away
-        # once ostro can handle on-the-fly scheduling, except for cases
-        # where we can't reach Valet at all, then we may opt to fail
-        # all hosts depending on a TBD config flag.
+        # TODO: If we can't reach Valet at all, we may opt to fail
+        # TODO: all hosts depending on a TBD config flag.
         if not filter_properties.get(hints_key, {}).has_key(uuid_key):
+            self._authorize()
             LOG.warn(_LW("Valet: Heat Stack Lifecycle Scheduler Hints " \
-                     "not found. Abstaining from placement."))
-            yield_all = True
+                     "not found. Performing ad-hoc placement."))
+            ad_hoc = True
+
+            # Beacuse this wasn't orchestrated, there's no stack.
+            # We're going to compose a resource as if there as one.
+            # In this particular case we assign the physical
+            # resource id to both the stack id and orchestration id.
+            request_spec = filter_properties.get('request_spec')
+            instance_properties = request_spec.get('instance_properties')
+            uuid = instance_properties.get('uuid')
+            instance_type = filter_properties.get('instance_type')
+            flavor = instance_type.get('name')
+
+            resources = {
+                uuid: {
+                    "properties": {
+                        "flavor": flavor,
+                    },
+                    "type": "OS::Nova::Server",
+                    "name": "ad_hoc_instance"
+                }
+            }
+
+            timeout = 60
+            plan = {
+                'plan_name': uuid,
+                'stack_id': uuid,
+                'timeout': '%d sec' % timeout,
+                'resources': resources
+            }
+            try:
+                response = self.api.plans_create( \
+                               None, plan, auth_token=self._auth_token)
+            except:
+                # TODO: Get context from exception
+                LOG.error(_LW("Valet did not respond to " \
+                          "ad hoc placement request."))
+                response = None
+
+            if response and response.get('placements'):
+                placements = response['placements']
+                if placements.get(uuid):
+                    placement = placements.get(uuid)
+                    location = placement['location']
+
+            if not location:
+                LOG.error(_LW("Valet ad-hoc placement unknown " \
+                          "for physical resource: %s.") % uuid)
+                yield_all = False
         else:
             uuid = filter_properties[hints_key][uuid_key]
             self._authorize()
             hosts = [obj.host for obj in filter_obj_list]
-            placement = self.api.placement(uuid, hosts=hosts,
-                                           auth_token=self._auth_token)
+
+            try:
+                placement = self.api.placement(uuid, hosts=hosts,
+                                               auth_token=self._auth_token)
+            except:
+                LOG.error(_LW("Valet did not respond to " \
+                          "placement request."))
+                placement = None
 
             # Ostro will give a matching format
             if placement and placement.get('location'):
                 location = placement['location']
 
             if not location:
+                # TODO: Get context from exception
                 LOG.error(_LW("Valet placement unknown " \
                           "for resource: %s.") % uuid)
                 yield_all = False
@@ -134,8 +187,12 @@ class ValetFilter(filters.BaseHostFilter):
             if location:
                 match = self._is_same_host(obj.host, location)
                 if match:
-                    LOG.info(_LI("Valet placement for resource %s: %s." % \
-                              (uuid, obj.host)))
+                    if ad_hoc:
+                        msg = "Valet ad-hoc placement for " \
+                              "physical resource %s: %s."
+                    else:
+                        msg = "Valet placement for resource %s: %s."
+                    LOG.info(_LI(msg % (uuid, obj.host)))
             else:
                 match = None
             if yield_all or match:
